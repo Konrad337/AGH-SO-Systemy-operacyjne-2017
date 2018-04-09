e2e35ea18e762b9250fb743d9e77689aa375e063
Konrad337 <konradrzonca@gmail.com>
2018-04-09 18:35:48 +0200

    Kind of finished cw04

--- cw04/task3/task.c
+++ cw04/task3/task.c
@@ -8,23 +8,21 @@
 #include <sys/wait.h>
 
 
-#define ADDITIONAL_INFO
 
 struct parsedArgs {
-    int childrenNum;
-    int childrenMin;
+    int L;
+    int Type;
 };
 
-int reqNum = 0;
-
+int pings = 0;
 
 struct parsedArgs parseArgs (int argc, char* argv[]) {
     if(argc != 3)
         exit(EXIT_FAILURE);
     struct parsedArgs args;
 
-    args.childrenNum = strtol(argv[1], NULL, 10);
-    args.childrenMin = strtol(argv[2], NULL, 10);
+    args.L = strtol(argv[1], NULL, 10);
+    args.Type = strtol(argv[2], NULL, 10);
 
     //printf("%i %i \n", args.childrenNum, args.childrenMin);
 
@@ -32,141 +30,192 @@ struct parsedArgs parseArgs (int argc, char* argv[]) {
     return args;
 }
 
+void doNothing(int sigN) { return; }
+
+
 void sigINT(int signum) {
+
+
+    struct sigaction finish;
+    finish.sa_handler = doNothing;
+    finish.sa_flags = 0;
+    sigemptyset (&finish.sa_mask);
+    sigaction(SIGUSR2, &finish, NULL);
+    kill(0, SIGUSR2);
+
+    printf("%c%cRecieved SIGINT - sent kill signal to child\n", 8, 8);
+
+
 
     errno = 0;
     while (errno != ECHILD)
        wait(NULL);
-    printf("%c%cOdebrano sygnał SIGINT\n", 8, 8);
+
     exit(EXIT_FAILURE);
 }
 
 
-void doNothing(int sigN) { return; }
-void recievePass(int sigN) { printf("Child with pid %i received pass\n", getpid()); }
-
-int childFun() {
 
 
+void dealWithPing(int signal, siginfo_t* info, void* ucontext) {
 
-    struct sigaction waitForSig;
-    waitForSig.sa_handler = doNothing;
-    waitForSig.sa_flags = 0;
-    sigaction(SIGUSR2, &waitForSig, NULL);
-
-    srand(getpid());
-    int sleepTime = rand() % 10;
-    sleep(sleepTime);
-
-    kill(getppid(), SIGUSR1);
-
-    sigset_t mask;
-    sigfillset(&mask);
-    sigdelset(&mask,SIGUSR2);
-    sigdelset(&mask,SIGINT);
-    sigsuspend(&mask);
-
-    kill(getppid(), rand() % (SIGRTMAX - SIGRTMIN) + SIGRTMIN);
-    return sleepTime;
-}
-
-void dealWithRequest(int sig) {
-    reqNum++;
+    pings++;
     #ifdef ADDITIONAL_INFO
-        printf("Got request number %i\n", reqNum);
+    printf("Child got pinged for %i time\n", pings);
     #endif
+    sleep(1);
+    kill(info -> si_pid, signal);
+
 }
 
-void dealWithFurtherRequest(int sig) {
 
+void dealWithPingParent(int signal, siginfo_t* info, void* ucontext) {
 
-    reqNum++;
-    kill(0, SIGUSR2);
+    pings++;
     #ifdef ADDITIONAL_INFO
-        printf("Got request number %i\n", reqNum);
-        //printf("Pass send to child\n");
+    printf("Got pinged from child, pinging it back\n");
+    #endif
+    kill(info -> si_pid, signal);
+
+}
+
+
+void printPingInfo(int signal, siginfo_t* info, void* ucontext) {
+
+    #ifdef ADDITIONAL_INFO
+    printf("Got pinged from child\n");
     #endif
 
 }
 
-void getPinged(int signal, siginfo_t* info, void* ucontext) {
-
-
-     #ifdef ADDITIONAL_INFO
-     printf("Got pinged from process %i with signal %i\n", info -> si_pid, signal);
-     #endif
-
+void exitChild(int sig) {
+    printf("Child recieved exit signal ENDING CHILD PROCESS\n");
+    exit(EXIT_SUCCESS);
 }
-
 
 int main( int argc, char* argv[] ) {
 
-    srand(time(NULL));
-
-    signal(SIGINT, sigINT);
-    signal(SIGUSR2, doNothing);
-    struct sigaction gettingReq;
-    gettingReq.sa_handler = dealWithRequest;
-    gettingReq.sa_flags = 0;
-    sigaction(SIGUSR1, &gettingReq, NULL);
 
 
-    struct sigaction gettingPinged;
-    for(int i = SIGRTMIN; i <= SIGRTMAX; i++) {
-        gettingPinged.sa_sigaction = getPinged;
-        gettingPinged.sa_flags = SA_SIGINFO;
-        sigaction(i, &gettingPinged, NULL);
-    }
+
 
     struct parsedArgs args = parseArgs(argc, argv);
-    int pid = 1;
-    int sleepTime;
 
-    for(int i = 0; i < args.childrenNum; i++) {
-        if (pid > 0) {
+    struct sigaction waitForChildSetUp;
+    struct sigaction prev;
+    waitForChildSetUp.sa_handler = doNothing;
+    waitForChildSetUp.sa_flags = 0;
+    sigaction(SIGUSR1, &waitForChildSetUp, &prev);
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask,SIGUSR1);
 
-            pid = fork();
 
-            #ifdef ADDITIONAL_INFO
-                if (pid > 0)
-                    printf("Created child process with pid: %i\n", pid);
-            #endif
+    int pid = 0;
+    pid = fork();
 
-            if (pid == 0)
-                sleepTime = childFun();
-            if (pid < 0)
-                exit(EXIT_FAILURE);
+    //printf("%i\n", pid);
 
-        }
-    }
 
+
+    if(pid == 0) {
+
+        struct sigaction getSIGINT;
+        getSIGINT.sa_handler = doNothing;
+        getSIGINT.sa_flags = 0;
+        sigset_t mask;
+        sigfillset(&mask);
+        sigdelset(&mask,SIGUSR2);
+        sigaction(SIGINT, &getSIGINT, NULL);
+
+
+
+        struct sigaction pinged;
+        pinged.sa_sigaction = dealWithPing;
+        pinged.sa_flags = SA_SIGINFO ;
+        sigemptyset (&pinged.sa_mask);
+        sigaction(SIGUSR1, &pinged, NULL);
+        if(args.Type == 3)
+            sigaction(SIGRTMIN + 1, &pinged, NULL);
+
+
+        struct sigaction finish;
+        finish.sa_handler = exitChild;
+        finish.sa_flags = 0;
+        sigemptyset (&finish.sa_mask);
+        sigaction(SIGUSR2, &finish, NULL);
+        if(args.Type == 3)
+            sigaction(SIGRTMIN + 2, &finish, NULL);
+
+
+        kill(getppid(), SIGUSR1);
+
+        while(pings < args.L);
+
+    } else
     if(pid > 0) {
 
-        while(reqNum < args.childrenMin);
+        signal(SIGINT, sigINT);
 
-        #ifdef ADDITIONAL_INFO
-        printf("Sending pass to children\n");
-        #endif
-
-        struct sigaction dealWithFurtherRequests;
-        dealWithFurtherRequests.sa_handler = dealWithFurtherRequest;
-        dealWithFurtherRequests.sa_flags = 0;
-        kill(0, SIGUSR2);
-        sigaction(SIGUSR1, &dealWithFurtherRequests, NULL);
+        sigsuspend(&mask);
+        sigaction(SIGUSR1, &prev, NULL);
 
 
+        if(args.Type == 1) {
 
-}
+
+            struct sigaction backPing;
+            backPing.sa_sigaction = printPingInfo;
+            backPing.sa_flags = SA_SIGINFO | SA_NODEFER;
+            sigaction(SIGUSR1, &backPing, NULL);
+
+            for(int i = 0; i < args.L; i++) {
+                kill(pid, SIGUSR1);
+                printf("Sent SIGUSR1 to child\n");
+                sleep(1);
+            }
+
+
+            kill(pid, SIGUSR2);
+            printf("Sent SIGUSR2 to child\n");
+
+        } else
+        if(args.Type == 2) {
+
+            struct sigaction backPing;
+            backPing.sa_sigaction = dealWithPingParent;
+            backPing.sa_flags = SA_SIGINFO | SA_NODEFER;
+            sigaction(SIGUSR1, &backPing, NULL);
+            kill(pid, SIGUSR1);
+            printf("Sent SIGUSR1 to child\n");
+            while(pings < args.L);
+            kill(pid, SIGUSR2);
+            printf("Sent SIGUSR2 to child\n");
+
+        } else
+        if(args.Type == 3) {
+
+            struct sigaction backPing;
+            backPing.sa_sigaction = printPingInfo;
+            backPing.sa_flags = SA_SIGINFO | SA_NODEFER;
+            sigaction(SIGRTMIN + 1, &backPing, NULL);
+
+            for(int i = 0; i < args.L; i++) {
+                kill(pid, SIGRTMIN + 1);
+                printf("Sent SIGRTMIN + 1 to child\n");
+                sleep(1);
+
+            }
+
+            kill(pid, SIGRTMIN + 2);
+            printf("Sent SIGRTMIN + 2 to child\n");
+
+        }
+    } else printf("No child created");
+
+
     errno = 0;
-    while (errno != ECHILD)
-       wait(NULL);
-
-    if(pid == 0)
-        printf("Child with pid %i ended work with sleep time %i\n", getpid(), sleepTime);
-
-
-
-
-
-
+    while (errno != ECHILD) {
+        wait(NULL);
+    }
 }
